5c7b78c8e3fa2c5ae209bf7b4f561841979312c0
MZCretin <mxnzp_life@163.com>
2018-09-06 14:15:09 +0800

    修改bug

--- app/src/main/java/com/cretin/www/expandabletextview/MainActivity.java
+++ app/src/main/java/com/cretin/www/expandabletextview/MainActivity.java
@@ -75,9 +75,9 @@ public class MainActivity extends AppCompatActivity {
         tvTips00 = findViewById(R.id.tv_tips00);
 
         setTips();
-
         String yourText = "  我所认识的中国，强大、友好。@奥特曼 “一带一路”经济带带动了沿线国家的经济发展，促进我国与他国的友好往来和贸易发展，可谓“双赢”。http://www.baidu.com 自古以来，中国以和平、友好的面孔示人。汉武帝派张骞出使西域，开辟丝绸之路，增进与西域各国的友好往来。http://www.baidu.com 胡麻、胡豆、香料等食材也随之传入中国，汇集于中华美食。@RNG 漠漠古道，驼铃阵阵，这条路奠定了“一带一路”的基础，让世界认识了中国。";
-//        String yourText = "1\n2\n3\n4\n5\n6\n7\n8";
+//
+//   String yourText = "1\n2\n3\n4\n5\n6\n7\n8";
         views[0].setContent(yourText);
         views[0].setLinkClickListener(linkClickListener);
 
