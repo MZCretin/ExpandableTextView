f82604c9a4766bd64a22894cc912c9f3f792161c
雷神 <mxnzp_life@163.com>
2019-05-29 10:07:54 +0800

    update demo

--- app/src/main/java/com/cretin/www/expandabletextview/MainActivity.java
+++ app/src/main/java/com/cretin/www/expandabletextview/MainActivity.java
@@ -189,6 +189,7 @@ public class MainActivity extends AppCompatActivity {
             yourText1 = "    我所认识的中国，强大、友好，[--习大大](schema_jump_userinfo)。[http://www.baidu.com](http://www.baidu.com)，@奥特曼 “一带一路”经济带带动了沿线国家的经济发展，促进我国与他国的友好往来和贸易发展，可谓“双赢”，[Github地址](https://github.com/MZCretin/ExpandableTextView)。http://www.baidu.com 自古以来，中国以和平、友好的面孔示人。汉武帝派张骞出使西域，开辟丝绸之路，增进与西域各国的友好往来。http://www.baidu.com 胡麻、胡豆、香料等食材也随之传入中国，汇集于中华美食。@RNG 漠漠古道，驼铃阵阵，这条路奠定了“一带一路”的基础，让世界认识了中国。";
         } else {
             tips[9].setText("10、正常带链接和@用户，有展开，有收回功能，带自定义规则");
+            yourText1 = yourText;
             setTips();
         }
         views[9].setContent(yourText1);
